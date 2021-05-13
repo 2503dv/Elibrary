adc2f42241f9e29a64d4f22f0f924e852a85acf3
2503dv <65950175+2503dv@users.noreply.github.com>
2021-05-13 14:35:15 +0530

    update

--- dbcollect.php
+++ dbcollect.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "sql210.epizy.com";
-$username1 = "epiz_28610295";
-$password1 = "yNScaO9qNn0";
-$database = "epiz_28610295_library";
+$servername ="localhost";
+$username1 = "root";
+$password1 = "";
+$database = "dhruvisign";
 
 $conn = mysqli_connect($servername,$username1,$password1,$database);
 if(!$conn){
